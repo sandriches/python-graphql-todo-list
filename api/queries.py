8ab72f010a15e908c73cedc8845ae9a02fc69520
sandriches <richard.corke@hotmail.co.uk>
2022-07-21 16:08:04 +0200

    add routes, models, mutations, queries, env

---
+++ api/queries.py
@@ -0,0 +1,41 @@
+from .models import Todo
+from ariadne import convert_kwargs_to_snake_case
+
+def resolve_todos(obj, info):
+    """
+    Fetches todos.
+
+    :obj: value returned by a parent resolver
+    :info: contains any context information that the GraphQL server provided the resolver during execution
+    :return: todo plus success boolean
+    """ 
+    try:
+        todos = [todo.to_dict() for todo in Todo.query.all()]
+        payload = {
+            "success": True,
+            "todos": todos
+        }
+    except Exception as error:
+        payload = {
+            "success": False,
+            "errors": [str(error)]
+        }
+    return payload
+
+
+@convert_kwargs_to_snake_case
+def resolve_todo(obj, info, todo_id):
+    try:
+        todo = Todo.query.get(todo_id)
+        payload = {
+            "success": True,
+            "todo": todo.to_dict()
+        }
+
+    except AttributeError:  # todo not found
+        payload = {
+            "success": False,
+            "errors": [f"Todo item matching id {todo_id} not found"]
+        }
+
+    return payload
